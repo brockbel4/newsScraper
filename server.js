f97c3ea04d330cad741ffd36ceba7413f90cbe13
Brock Belmonte <brockbel@gmail.com>
2018-06-04 14:42:13 -0500

    Linking the DB to Heroku

--- server.js
+++ server.js
@@ -20,7 +20,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/scrapedNewsDB");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapedNewsDB";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// mongoose.connect("mongodb://localhost/scrapedNewsDB");
 
 app.get("/articles", function (req, res) {
 
